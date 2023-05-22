c04923655911b3a2044f9260d17c55dd1e4ab5e0
Javailabe <kulecki.pawel@gmail.com>
2023-05-22 22:32:53 +0200

    lombok dependency added, base participant created

---
+++ src/main/java/com/eventer_v2/model/Participant.java
@@ -0,0 +1,16 @@
+package com.eventer_v2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Participant {
+
+    private String name;
+    private String surname;
+    private String email;
+    
+}
